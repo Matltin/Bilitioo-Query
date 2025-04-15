38ac78e24eb86280cfdc258fc8b1a21d6f0e4d6a
ryhn-mir <Rmirarabshahi2005@gmail.com>
2025-04-15 13:19:33 +0330

    3 & 4 queries

--- queries/query.sql
+++ queries/query.sql
@@ -26,3 +26,35 @@
     GROUP BY u.id, pro.first_name, pro.last_name, u.role
     HAVING COUNT(r.ticket_id) > 0
     ORDER BY u.id;
+
+-- 3)
+    SELECT 
+        u.id, 
+        CONCAT(pro.first_name, ' ', pro.last_name) AS full_name,
+        TO_CHAR(pa.created_at, 'YYYY-MM') AS payment_month,
+        SUM(pa.amount) AS total_amount
+    FROM "user" u 
+    INNER JOIN "reservation" r ON u.id = r.user_id
+    LEFT JOIN "payment" pa ON r.payment_id = pa.id
+    JOIN "profile" pro ON u.id = pro.user_id
+    GROUP BY u.id, pro.first_name, pro.last_name, TO_CHAR(pa.created_at, 'YYYY-MM')
+    HAVING SUM(pa.amount) IS NOT NULL
+    ORDER BY u.id, payment_month;
+
+-- 4)
+    SELECT 
+        u.id,
+        CONCAT(pro.first_name, ' ', pro.last_name) AS full_name,
+        CONCAT(dc.province, '(', dc.county, ')') AS destination,
+        COUNT(dc.county) AS "tripNO."
+    FROM "user" u
+    INNER JOIN "profile" pro ON u.id = pro.user_id
+    INNER JOIN "reservation" re ON u.id = re.user_id
+    INNER JOIN "ticket" t ON re.ticket_id = t.id
+    INNER JOIN "route" ro ON t.route_id = ro.id
+    INNER JOIN "city" oc ON oc.id = ro.origin_city_id
+    INNER JOIN "city" dc ON dc.id = ro.destination_city_id
+    GROUP BY u.id, pro.first_name, pro.last_name, dc.county, dc.province
+    HAVING COUNT(dc.county) = 1
+    ORDER BY u.id;
+
