d10cfbdefbbc8783e907be6cecd22ac806054a7f
Matltin <ma1383ahmadi@gmail.com>
2025-04-29 15:06:16 +0330

    add main test

--- test/main_test.go
+++ test/main_test.go
@@ -1,2 +1,22 @@
 package db
 
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	testDB, err := sql.Open("postgres", "postgresql://root:secret@localhost:5432/test_for_bilitioo_db?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
